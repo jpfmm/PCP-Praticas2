e46654ee5cc0b8eb6199d0860d69529615fe8dbe
jpfmm <jpfmm123@gmail.com>
2014-10-14 22:51:31 +0100

    pi 2.1

--- PiF.c
+++ PiF.c
@@ -16,9 +16,13 @@ Source code:*/
 
 
 int main(int argc, char**){
-   double pi, niter, count_local, count, x, y, z;
+   double pi, count_local, x, y, z;
    int rank_W, size_W;
 
+   if(argc != 2){
+	printf("Argumentos invalidos");
+   }
+   
    /* initialize random numbers */
    srand(SEED);
    count = 0;
@@ -27,10 +31,12 @@ int main(int argc, char**){
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank_W);
 	MPI_Comm_size(MPI_COMM_WORLD, &size_W);
 
-	MPI_Bcast(&niter, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	long count, niter = atol(argv[1]) / size_W; 
+	
+	MPI_Bcast(&niter, 1, MPI_LONG, 0, MPI_COMM_WORLD);
 	
 	count_local = 0;
-	for ( i=0; i<(niter%size_W); i++) {
+	for ( i = 0; i < niter; i++) {
       x = (double)rand()/RAND_MAX;
       y = (double)rand()/RAND_MAX;
       z = x*x+y*y;
@@ -40,11 +46,10 @@ int main(int argc, char**){
 	MPI_Reduce(&count_local, &count, 1, MPI_DOUBLE, MPI_SUM,0, MPI_COMM_WORLD);
 	
 	if(rank_W == 0){
-		pi = 4*count/niter;
+		pi = 4.0*(double)count/(niter*size_W);
 		printf("Valor estimado de pi: %g para %g iteracoes.\n", pi, niter);
 	}
 	
 	MPI_Finalize();
-	
 	return 0;
 }
