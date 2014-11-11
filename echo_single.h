1c402e005d43529fc2d78c5bb0a3d042c2cc3794
jpfmm <jpfmm123@gmail.com>
2014-11-11 01:59:20 +0000

    addaad

---
+++ echo_single.h
@@ -0,0 +1,15 @@
+//
+//  echo_single.h
+//  Echo
+//
+//  Created by Hélder José Alves Gonçalves on 09/11/14.
+//  Copyright (c) 2014 Hélder José Alves Gonçalves. All rights reserved.
+//
+
+#ifndef Echo_echo_single_h
+#define Echo_echo_single_h
+
+void echoSingle();
+
+
+#endif
